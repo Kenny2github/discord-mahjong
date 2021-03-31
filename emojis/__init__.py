b5787da0712e589cb395121e7d511f4c56dc579f
Kenny2github <kenny2minecraft@gmail.com>
2021-03-31 19:03:59 +0800

    Rename emoji→emojis and add py for it

---
+++ emojis/__init__.py
@@ -0,0 +1,20 @@
+import os
+import json
+
+__all__ = ['tile', 'tiles']
+
+with open(os.path.join('emojis', 'emojis.json')) as f:
+    EMOJIS = json.load(f)
+
+def tile(path):
+    # if path is already string, it's unaffected
+    # if it's a Tile, it's cast to a path
+    suit, number = str(path).split('/')
+    if number.isdecimal():
+        number = int(number)
+    return EMOJIS[suit][number]
+
+def tiles(paths):
+    if not isinstance(paths, (list, tuple)):
+        paths = str(paths).split('|')
+    return ''.join(tile(path) for path in paths)
