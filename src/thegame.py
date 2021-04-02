38cdc652425ab7dc52c7fa16e6e575a47f64e1ff
Kenny2github <kenny2minecraft@gmail.com>
2021-04-03 03:26:37 +0800

    Initial implementation of full game loop

--- src/thegame.py
+++ src/thegame.py
@@ -4,6 +4,7 @@ import asyncio
 import mahjong
 import discord
 import emojis
+from i18n import i18n
 
 Person = Union[discord.User, discord.Member]
 EMOJI_LINE_LIMIT = 12
@@ -63,6 +64,93 @@ async def react_hand(player: Person):
         await msg2.add_reaction(i)
     return (msg1, msg2)
 
+async def wait_react(
+    msg: discord.Message, bot: discord.ext.commands.Bot,
+    allowed: Tuple[str, ...] = None, timeout=10.0
+):
+    fut = asyncio.get_running_loop().create_future()
+    async def listener(payload: discord.RawReactionActionEvent):
+        if allowed is not None and str(payload.emoji) not in allowed:
+            return
+        if payload.message_id == msg.id and payload.user_id != bot.user.id:
+            fut.set_result(str(payload.emoji))
+    bot.add_listener(listener, 'on_raw_reaction_add')
+    bot.add_listener(listener, 'on_raw_reaction_remove')
+    r = await fut # await asyncio.wait_for(fut, timeout)
+    bot.remove_listener(listener, 'on_raw_reaction_add')
+    bot.remove_listener(listener, 'on_raw_reaction_remove')
+    return str(r)
+
+async def discard_what(
+    question: mahjong.UserIO, player: Person,
+    msgs: Tuple[discord.Message, discord.Message],
+    bot: discord.ext.commands.Bot
+) -> mahjong.Tile:
+    msg = await player.send(embed=discord.Embed(
+        description=i18n(player, 'q-discard-what')))
+    emoji = await wait_react(
+        msgs[1], bot, tuple(emojis.LETTERS[:len(question.playable_hand)]))
+    background(msg.delete())
+    return question.playable_hand[emojis.LETTERS.index(emoji)]
+
+async def meld_from_discard_q(question: mahjong.UserIO, player: Person,
+                              bot: discord.ext.commands.Bot):
+    meld_types = tuple({type(meld) for meld in question.melds})
+    desc = ''
+    for i, typ in enumerate(meld_types):
+        desc += i18n(player, 'meld-type-' + typ.__name__,
+                     emojis.number(i+1)) + '\n'
+    desc += i18n(player, 'meld-type-None', emojis.CANCEL)
+    foot = i18n(player, 'q-meld-from-discard-q')
+    msg = await player.send(
+        embed=discord.Embed(description=desc).set_footer(text=foot))
+    buttons = tuple(map(emojis.number, range(1, len(meld_types) + 1))) \
+        + (emojis.CANCEL,)
+    for r in buttons:
+        await msg.add_reaction(r)
+    emoji = await wait_react(msg, bot, buttons)
+    try:
+        typ = meld_types[emojis.NUMBERS.index(emoji) - 1]
+    except ValueError:
+        return None
+    melds = [meld for meld in question.melds if isinstance(meld, typ)]
+    if len(melds) == 1:
+        background(msg.delete())
+        return melds[0]
+    desc = ''
+    for i, meld in enumerate(melds):
+        desc += f'{emojis.number(i+1)}: {emojis.tiles(meld)}\n'
+    desc += i18n(player, 'meld-type-None', emojis.CANCEL)
+    foot = i18n(player, 'q-which-meld')
+    background(msg.edit(
+        embed=discord.Embed(description=desc).set_footer(text=foot)))
+    buttons2 = tuple(map(emojis.number, range(1, len(melds) + 1))) \
+        + (emojis.CANCEL,)
+    background(msg.remove_reaction(emojis.CANCEL, discord.Object(bot.user.id)))
+    for r in buttons2:
+        if r in buttons and r != emojis.CANCEL:
+            continue # already reacted
+        await msg.add_reaction(r)
+    emoji = await wait_react(msg, bot, buttons2)
+    background(msg.delete())
+    try:
+        return melds[emojis.NUMBERS.index(emoji) - 1]
+    except ValueError:
+        return None
+
+async def rob_kong_q(question: mahjong.UserIO, player: Person,
+                     bot: discord.ext.commands.Bot):
+    if question.question == mahjong.Question.ROB_KONG_Q:
+        key = 'q-rob-kong-q'
+    else:
+        key = 'q-self-draw-q'
+    msg = await player.send(embed=discord.Embed(description=i18n(
+        player, key, emojis.tiles(question.melds[0]))))
+    background(msg.add_reaction(emojis.CHECK))
+    background(msg.add_reaction(emojis.CANCEL))
+    emoji = await wait_react(msg, bot, (emojis.CHECK, emojis.CANCEL))
+    return emoji == emojis.CHECK
+
 async def play(players: Tuple[Person, ...], bot: discord.ext.commands.Bot):
     game = mahjong.Hand(None)
     prevailing = mahjong.Wind(random.randrange(4))
@@ -81,10 +169,24 @@ async def play(players: Tuple[Person, ...], bot: discord.ext.commands.Bot):
             hand_msg = hand_msgs[question.player.seat.value]
             background(update_hand(hand_msg, question))
             if question.question == mahjong.Question.DISCARD_WHAT:
-                asyncio.create_task(player.send(
-                    str_hand(question)
-                    #embed=discord.Embed(
-                    #    description=str_hand(question)
-                    #)
-                )) # TODO
-            return
+                tile = await discard_what(question, player, hand_msg, bot)
+                question = await answer(question, tile)
+            elif question.question in {
+                mahjong.Question.MELD_FROM_DISCARD_Q,
+                mahjong.Question.SHOW_EKFCP_Q,
+                mahjong.Question.SHOW_EKFEP_Q
+            }:
+                meld = await meld_from_discard_q(question, player, bot)
+                question = await answer(question, meld)
+            elif question.question in {
+                mahjong.Question.ROB_KONG_Q,
+                mahjong.Question.SELF_DRAW_Q
+            }:
+                do = await rob_kong_q(question, player, bot)
+                question = await answer(question, do)
+            elif question.question == mahjong.Question.WHICH_WU:
+                choice = max(question.melds, key=lambda m:
+                    mahjong.Wu(melds=m).faan())
+                question = await answer(question, choice)
+        elif isinstance(question, mahjong.HandEnding):
+            question = await answer(question)
